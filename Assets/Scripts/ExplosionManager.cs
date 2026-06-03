0172c17df23d7afe941fdf46db2e5fe3687a13d6
Callum Nicholson <nicholson.callum@spark-creative.co.jp>
2026-06-03 11:54:46 +0900

    Moved to world coords

--- Assets/Scripts/ExplosionManager.cs
+++ Assets/Scripts/ExplosionManager.cs
@@ -3,8 +3,11 @@ using UnityEngine.InputSystem;
 
 public class ExplosionManager : MonoBehaviour
 {
-    public ComputeShader fluidSimCompute;
-    public int resolution = 64;
+    [SerializeField] private ComputeShader fluidSimCompute;
+    [SerializeField] private int resolution = 64;
+    [SerializeField] private float simScale = 1.0f;
+    [SerializeField] private float injectRadius = 1.5f;
+    [SerializeField] private Transform injectionPoint = null;
 
     private RenderTexture volumeTexture;
     private Material rayMarchMaterial;
@@ -25,30 +28,45 @@ public class ExplosionManager : MonoBehaviour
         injectKernel = fluidSimCompute.FindKernel("InjectDensity");
         simulateKernel = fluidSimCompute.FindKernel("Simulate");
 
+        // Set Parameters
         fluidSimCompute.SetInt("Resolution", resolution);
 
-        // 1. Clear the garbage memory out of the texture
+        // Bind texture
         fluidSimCompute.SetTexture(initKernel, "VolumeTexture", volumeTexture);
-        fluidSimCompute.Dispatch(initKernel, resolution / 8, resolution / 8, resolution / 8);
-
-        // 2. Bind to other kernels
         fluidSimCompute.SetTexture(injectKernel, "VolumeTexture", volumeTexture);
         fluidSimCompute.SetTexture(simulateKernel, "VolumeTexture", volumeTexture);
 
         rayMarchMaterial = GetComponent<Renderer>().material;
         rayMarchMaterial.SetTexture("_VolumeTex", volumeTexture);
+
+        // Clear memory
+        // TODO // Poll the thread group size instead
+        fluidSimCompute.Dispatch(initKernel, resolution / 8, resolution / 8, resolution / 8);
+
     }
 
     void Update()
     {
+        Bounds bounds = GetComponent<Renderer>().bounds;
+
         fluidSimCompute.SetFloat("Time", Time.time);
         fluidSimCompute.SetFloat("DeltaTime", Time.deltaTime);
+        fluidSimCompute.SetFloat("SimScale", simScale);
+        fluidSimCompute.SetVector("BoundsMin", bounds.min);
+        fluidSimCompute.SetVector("BoundsSize", bounds.size);
 
         if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
         {
+            Vector3 injectPos = injectionPoint != null ? injectionPoint.position : transform.position;
+
+            fluidSimCompute.SetVector("InjectWorldPos", injectPos);
+            fluidSimCompute.SetFloat("InjectRadius", injectRadius);
+
+            // TODO // Poll the thread group size instead
             fluidSimCompute.Dispatch(injectKernel, resolution / 8, resolution / 8, resolution /8);
         }
 
+        // TODO // Poll the thread group size instead
         fluidSimCompute.Dispatch(simulateKernel, resolution / 8, resolution / 8, resolution / 8);
     }
 
